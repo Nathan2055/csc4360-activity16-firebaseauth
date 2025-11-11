73028e37fd3a4bb30223698bdb8d8a5162b127c7
Nathan Larsen <nlarsen@nathan2055.com>
2025-11-11 18:38:19 -0500

    Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'firebase_options.dart';
+import 'package:firebaseauth/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
